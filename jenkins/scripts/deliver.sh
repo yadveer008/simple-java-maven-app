2da490a227cd69194d0de9b15c3f75348006bee0
root <root@ip-172-31-58-33.ec2.internal>
2020-09-23 02:14:27 +0000

    changed the message

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo 'The following Maven command installs your Maven-built Java application..'
-echo 'into the local Maven repository, which will ultimately be stored in'
+echo 'into the local Maven repository, which will ultimately be stored in..'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
 set -x
